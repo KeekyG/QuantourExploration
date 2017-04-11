5bd61f765ef9342911de4045ff6554b3409d185b
151250151 <151250151@smail.nju.edu.cn>
2017-04-12 01:59:54 +0800

    不学了

--- src/main/java/vo/HoldingPeriodStockVO.java
+++ src/main/java/vo/HoldingPeriodStockVO.java
@@ -5,10 +5,20 @@ import java.util.HashMap;
 
 public class HoldingPeriodStockVO extends StockSetVO {
 	
+	private HashMap<LocalDate, Double> rates;
+
 	public HoldingPeriodStockVO(LocalDate beginDate, LocalDate endDate, HashMap<String, ShareLineVO> stocks) {
 		super();
 		super.setBeginDate(beginDate);
 		super.setEndDate(endDate);
 		super.setStocks(stocks);
+	}
+
+	public HashMap<LocalDate, Double> getRates() {
+		return rates;
+	}
+	
+	public void setRates(HashMap<LocalDate, Double> rates) {
+		this.rates = rates;
 	}
 }
