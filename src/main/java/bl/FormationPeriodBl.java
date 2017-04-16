5cea30315559f772f48c54aaa8e370f466853dc1
151250151 <151250151@smail.nju.edu.cn>
2017-04-16 17:37:26 +0800

    我日你妈

--- src/main/java/bl/FormationPeriodBl.java
+++ src/main/java/bl/FormationPeriodBl.java
@@ -2,13 +2,17 @@ package bl;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import blService.FormationPeriodBlService;
+import utility.TimeUtility;
 import vo.FormationPeriodStockVO;
 import vo.ShareLineVO;
 import vo.StockPoolVO;
+import vo.StockShareVO;
 
 public class FormationPeriodBl implements FormationPeriodBlService {
 
@@ -24,7 +28,7 @@ public class FormationPeriodBl implements FormationPeriodBlService {
 		int size = dateList.size();
 		int times = size / (formationDays + holdingDays);
 		for (int i = 0; i < size; i+=(formationDays+holdingDays)) {
-			List<LocalDate> subList = dateList.subList(i, i+formationDays);
+			List<LocalDate> subList = dateList.subList(i, i+formationDays+holdingDays);
 			ArrayList<LocalDate> innerList = new ArrayList<>();
 			for (LocalDate localDate : subList) {
 				innerList.add(localDate);
@@ -42,8 +46,28 @@ public class FormationPeriodBl implements FormationPeriodBlService {
 			FormationPeriodStockVO formationPeriodStockVO;
 			HashMap<String, ShareLineVO> stockMap = new HashMap<>();
 			
+			Iterator<String> iterator = stocksInPool.keySet().iterator();
+			while (iterator.hasNext()) {
+				String code = (String) iterator.next();
+				ShareLineVO singleStockLine = stocksInPool.get(code);
+				int index = -1;
+				for (StockShareVO stockShareVO : singleStockLine) {
+					Date date = TimeUtility.localDateToDate(period.get(0));
+					if (stockShareVO.getDate().equals(date)) {
+						index = singleStockLine.indexOf(stockShareVO);
+					}
+				}
+				if (index > -1 && (index + formationDays) <= singleStockLine.size() && TimeUtility.localDateToDate(period.get(period.size()-1)).equals(singleStockLine.get(index+formationDays+holdingDays-1))) {
+					ArrayList<StockShareVO> formationStocks = new ArrayList<>();
+					for (int i = index; i < index + formationDays; i++) {
+						formationStocks.add(singleStockLine.get(i));
+					}
+					stockMap.put(code, new ShareLineVO(formationStocks, singleStockLine.getBeginDate(), singleStockLine.getEndDate(), code, singleStockLine.getName(), singleStockLine.getYesterdayShare()));
+				}
+			}
 			
-			
+			formationPeriodStockVO = new FormationPeriodStockVO(period.get(0), period.get(period.size()-1), stockMap);
+			fomationPeriods.add(formationPeriodStockVO);
 		}
 		
 		
