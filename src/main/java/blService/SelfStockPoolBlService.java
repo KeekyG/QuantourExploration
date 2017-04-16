474b7588aa36a72821624d8ff0627bc3872d9cb4
151250151 <151250151@smail.nju.edu.cn>
2017-04-16 14:26:06 +0800

    宋少威傻逼

---
+++ src/main/java/blService/SelfStockPoolBlService.java
@@ -0,0 +1,52 @@
+package blService;
+
+import java.util.ArrayList;
+
+import vo.SelfSelectStockPoolVO;
+
+public interface SelfStockPoolBlService {
+	
+	/**
+	 * 获取所有自选股票池名称
+	 * @return 所有自选股票池名称
+	 */
+	public ArrayList<String> getStockPools();
+	
+	/**
+	 * 获取一个股票池里所有股票
+	 * @param name 股票池名称
+	 * @return 自选股票池VO
+	 */
+	public SelfSelectStockPoolVO getStocks(String name);
+	
+	/**
+	 * 增加股票池
+	 * @param name 股票池名称
+	 * @return 是否成功
+	 */
+	public boolean addStockPool(String name);
+	
+	/**
+	 * 往股票池中添加股票
+	 * @param stockPoolName 股票池名称
+	 * @param stockCode 股票代码
+	 * @param stockName 股票名称
+	 * @return 是否成功
+	 */
+	public boolean addStock(String stockPoolName, String stockCode, String stockName);
+	
+	/**
+	 * 删除股票池
+	 * @param name 股票池名称
+	 * @return 是否成功
+	 */
+	public boolean deleteStockPool();
+	
+	/**
+	 * 删除股票池中的股票
+	 * @param stockPoolName 股票池名称
+	 * @param stockCode 股票代码
+	 * @return 是否成功
+	 */
+	public boolean deleteStock(String stockPoolName, String stockCode);
+}
