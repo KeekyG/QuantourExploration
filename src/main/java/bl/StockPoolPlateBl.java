3df751e42e14a05a08350ec488bae495b9b4c1e7
151250151 <151250151@smail.nju.edu.cn>
2017-04-17 11:15:02 +0800

    哎我日

--- src/main/java/bl/StockPoolPlateBl.java
+++ src/main/java/bl/StockPoolPlateBl.java
@@ -1,8 +1,19 @@
 package bl;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
+import blService.BenchmarkBlService;
+import blService.StockBlService;
 import blService.StockPoolBlService;
+import data.dao.PoolCodeDao;
+import data.dao.impl.PoolCodeDaoImpl;
+import enums.PlateType;
+import enums.StockPoolType;
+import utility.TimeUtility;
+import vo.IncomeLineVO;
+import vo.PointVO;
+import vo.ShareLineVO;
 import vo.StockPoolVO;
 
 /**
@@ -13,10 +24,46 @@ import vo.StockPoolVO;
  */
 public class StockPoolPlateBl implements StockPoolBlService {
 
+	private PoolCodeDao poolCodeDao;
+	private StockBlService stockBl;
+	private BenchmarkBlService benchmarkBl;
+	
+	public StockPoolPlateBl() {
+		this.poolCodeDao = new PoolCodeDaoImpl();
+		this.stockBl = new StockBl();
+		this.benchmarkBl = new BenchmarkBl();
+	}
+	
 	@Override
-	public StockPoolVO getStockPool(String name, LocalDate beginDate, LocalDate enDate) {
-		// TODO Auto-generated method stub
-		return null;
+	public StockPoolVO getStockPool(String name, LocalDate beginDate, LocalDate endDate) {
+		StockPoolVO stockPoolVO;
+		ArrayList<String> codeList = new ArrayList<>();
+		IncomeLineVO incomeLineVO = null;
+		if (name.equals("HS300")) {
+			codeList = poolCodeDao.getHS300Code();
+			incomeLineVO = benchmarkBl.getBenchmark(PlateType.HS300, beginDate, endDate);
+		} else if (name.equals("SME")) {
+			codeList = poolCodeDao.getSMEcode();
+			incomeLineVO = benchmarkBl.getBenchmark(PlateType.SME, beginDate, endDate);
+		} else if (name.equals("GEM")) {
+			codeList = poolCodeDao.getGEMcode();
+			incomeLineVO = benchmarkBl.getBenchmark(PlateType.GEM, beginDate, endDate);
+		}
+		
+		ArrayList<ShareLineVO> poolStocks = new ArrayList<>();
+		
+		for (String code : codeList) {
+			poolStocks.add(stockBl.getShareLine(TimeUtility.localDateToDate(beginDate), TimeUtility.localDateToDate(endDate), code));
+		}
+		
+		ArrayList<LocalDate> dateList = new ArrayList<>();
+		for (PointVO pointVO : incomeLineVO) {
+			dateList.add(pointVO.getDate());
+		}
+		
+		stockPoolVO = new StockPoolVO(StockPoolType.PLATE, name, beginDate, endDate, poolStocks, dateList);
+		
+		return stockPoolVO;
 	}
 
 }
