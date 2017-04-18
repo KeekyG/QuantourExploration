ad0cabc7ced4f51d4d5efb9f9d6127b01c69d598
151250151 <151250151@smail.nju.edu.cn>
2017-04-18 19:15:58 +0800

    希望你喜欢我的新发明

--- src/main/java/bl/IncomeBl.java
+++ src/main/java/bl/IncomeBl.java
@@ -10,17 +10,21 @@ import utility.TimeUtility;
 import vo.HoldingPeriodStockVO;
 import vo.IncomeLineVO;
 import vo.ShareLineVO;
+import vo.StockPoolVO;
+import vo.StockShareVO;
 
 public class IncomeBl implements IncomeBlService {
 
 	
 	@Override
-	public IncomeLineVO calculateTotalIncomeRate(ArrayList<HoldingPeriodStockVO> holdingPeriodStocks) {
+	public IncomeLineVO calculateIncomeRate(ArrayList<HoldingPeriodStockVO> holdingPeriodStocks) {
 		HashMap<LocalDate, Double> incomeMap = new HashMap<>();
-		int size = holdingPeriodStocks.get(0).getStocks().size();
+		
 		for (HoldingPeriodStockVO holdingPeriodStockVO : holdingPeriodStocks) {
 			
 			HashMap<String, ShareLineVO> stockMap = holdingPeriodStockVO.getStocks();
+			
+			double size = stockMap.size();
 			
 			Iterator<String> iterator = stockMap.keySet().iterator();
 			while (iterator.hasNext()) {
@@ -29,14 +33,14 @@ public class IncomeBl implements IncomeBlService {
 				for (int i = 0; i < shareLineVO.size(); i++) {
 					LocalDate date = TimeUtility.dateToLocalDate(shareLineVO.get(i).getDate());
 					if (i == 0) {
-						double rate = (shareLineVO.get(i).getAdjClose() - shareLineVO.get(i).getOpen()) / shareLineVO.get(i).getOpen();
+						double rate = (shareLineVO.get(i).getAdjClose() - shareLineVO.get(i).getOpen()) / shareLineVO.get(i).getOpen() / size;
 						if (incomeMap.containsKey(date)) {
 							incomeMap.put(date, incomeMap.get(date) + rate);
 						} else {
 							incomeMap.put(date, (shareLineVO.get(i).getAdjClose() - shareLineVO.get(i).getOpen()) / shareLineVO.get(i).getOpen());
 						}
 					} else {
-						double rate = (shareLineVO.get(i).getAdjClose() - shareLineVO.get(i-1).getAdjClose()) / shareLineVO.get(i-1).getAdjClose();
+						double rate = (shareLineVO.get(i).getAdjClose() - shareLineVO.get(i-1).getAdjClose()) / shareLineVO.get(i-1).getAdjClose() / size;
 						if (incomeMap.containsKey(date)) {
 							incomeMap.put(date, incomeMap.get(date) + rate);
 						} else {
@@ -47,16 +51,47 @@ public class IncomeBl implements IncomeBlService {
 			}
 			
 		}
-		return null;
+		return calculateTotalRate(new IncomeLineVO(incomeMap));
 	}
 
 	@Override
-	public IncomeLineVO calculateTotalBenchmarkRate(IncomeLineVO incomeLineVO) {
+	public IncomeLineVO calculateTotalRate(IncomeLineVO incomeLineVO) {
 		int size = incomeLineVO.size();
 		for (int i = 1; i < size; i++) {
 			incomeLineVO.get(i).setRate(incomeLineVO.get(i-1).getRate() * incomeLineVO.get(i).getRate());
 		}
 		return incomeLineVO;
+	}
+
+	@Override
+	public IncomeLineVO calculateAverageIncome(StockPoolVO stockPoolVO) {
+		HashMap<LocalDate, Double> incomeMap = new HashMap<>();
+		HashMap<LocalDate, Integer> volumeMap = new HashMap<>();
+		for (LocalDate date : stockPoolVO.getDateList()) {
+			incomeMap.put(date, 0.0);
+			volumeMap.put(date, 0);
+		}
+		
+		HashMap<String, ShareLineVO> stockMap = stockPoolVO.getStocks();
+		Iterator<String> codeIterator = stockMap.keySet().iterator();
+		while (codeIterator.hasNext()) {
+			String code = (String) codeIterator.next();
+			ShareLineVO shareLineVO = stockMap.get(code);
+			for (StockShareVO stockShareVO : shareLineVO) {
+				LocalDate date = TimeUtility.dateToLocalDate(stockShareVO.getDate());
+				double rate = (stockShareVO.getAdjClose() - stockShareVO.getOpen()) / stockShareVO.getOpen();
+				incomeMap.put(date, incomeMap.get(date)+rate);
+				volumeMap.put(date, volumeMap.get(date)+1);
+			}
+		}
+		
+		Iterator<LocalDate> dateIterator = incomeMap.keySet().iterator();
+		while (dateIterator.hasNext()) {
+			LocalDate localDate = (LocalDate) dateIterator.next();
+			incomeMap.put(localDate, incomeMap.get(localDate) / (double) volumeMap.get(localDate));
+		}
+		
+		return calculateTotalRate(new IncomeLineVO(incomeMap));
 	}
 
 }
--- src/main/java/blService/IncomeBlService.java
+++ src/main/java/blService/IncomeBlService.java
@@ -1,9 +1,11 @@
 package blService;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 import vo.HoldingPeriodStockVO;
 import vo.IncomeLineVO;
+import vo.StockPoolVO;
 
 public interface IncomeBlService {
 	
@@ -12,14 +14,21 @@ public interface IncomeBlService {
 	 * @param holdingPeriodStocks 持有期
 	 * @return 累计持有期收益率
 	 */
-	public IncomeLineVO calculateTotalIncomeRate(ArrayList<HoldingPeriodStockVO> holdingPeriodStocks);
+	public IncomeLineVO calculateIncomeRate(ArrayList<HoldingPeriodStockVO> holdingPeriodStocks);
 	
 	/**
-	 * 通过形成期和收益率计算基准收益率的累计收益率
-	 * @param holdingPeriodStocks 持有期
+	 * 计算股票池的平均收益作为基准收益
+	 * @param stockPoolVO 股票池
+	 * @return 累计收益
+	 */
+	public IncomeLineVO calculateAverageIncome(StockPoolVO stockPoolVO);
+	
+	/**
+	 * 计算累计收益率
 	 * @param incomeLineVO 每天的收益
 	 * @return 累计基准收益率
 	 */
-	public IncomeLineVO calculateTotalBenchmarkRate(IncomeLineVO incomeLineVO);
+	public IncomeLineVO calculateTotalRate(IncomeLineVO incomeLineVO);
+	
 	
 }
